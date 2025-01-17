c61d7e43b05ee85f6d88e593a750ec63c681f792
Fredi R. Mino <35937021+Fredware@users.noreply.github.com>
2025-01-17 15:21:50 -0700

    Working version of pwm_demo for BLDC motor

--- pwm_demo/pwm_demo.ino
+++ pwm_demo/pwm_demo.ino
@@ -1,22 +1,39 @@
-#define PWM_PIN 10 //OC2A
+#define PWM_DIR_PIN 12
+#define PWM_OUT_PIN 10 //OC2A
+#define PWM_BRK_PIN 8
 
-unsigned int pwm_val = 0;
+
+unsigned int pwm_val = 10;
+int pwm_increment = 10;
 
 void setup() {
-  pinMode(PWM_PIN, OUTPUT);
-  /*Non-prescaled PWM: =~ 31.3kHz*/
+    /*Non-prescaled PWM: =~ 31.3kHz*/
   TCCR2A = _BV(COM2A1) | _BV(COM2A0) | _BV(COM2B1)  | _BV(COM2B0) | _BV(WGM20);
   TCCR2B = _BV(CS20);
   Serial.begin(115200);
+
+  pinMode(PWM_DIR_PIN, OUTPUT);
+  digitalWrite(PWM_DIR_PIN, HIGH);
+
+  pinMode(PWM_BRK_PIN, OUTPUT);
+  digitalWrite(PWM_BRK_PIN, LOW); /*HIGH = BREAK; LOW = CONTINUE*/
+
+  pinMode(PWM_OUT_PIN, OUTPUT);
+  analogWrite(PWM_OUT_PIN, 1); /*Ideally 0, but 0 = MAX_SPEED = 255*/
 }
 
 void loop() 
 {
-  analogWrite(PWM_PIN, pwm_val);
-  delay(3000);
-  pwm_val = 100;
-  // if (pwm_val >= 255) {
-  //   pwm_val = 0;
-  // }
+  pwm_val += pwm_increment;
+  if (pwm_val >= 250) {
+    pwm_val = 250;
+    pwm_increment = -10; 
+  }
+  if (pwm_val <= 40){
+    pwm_val = 40;
+    pwm_increment = 10;
+  }
   Serial.println(pwm_val);
+  analogWrite(PWM_OUT_PIN, pwm_val);
+  delay(1500);
 }
--- torque_control_demo/torque_control_demo.ino
+++ torque_control_demo/torque_control_demo.ino
@@ -11,6 +11,7 @@
 #define BAUD_RATE 115200
 
 #define ADC_PIN A8
+#define POT_PIN A0
 
 #define ANGLE_REGISTER_ADDRESS 0x0E
 #define ENCODER_ADDRESS 0x36
@@ -66,6 +67,8 @@ void setup(){
     pinMode(LOOP_RATE_PIN, OUTPUT);
     /*ADC setup*/
     pinMode(ADC_PIN, INPUT);
+    /*Configfure to read setpoint from analog pot*/
+    pinMode(POT_PIN, INPUT);
     /*PWM setup*/
     pinMode(PWM_DIR_PIN, OUTPUT);
     pinMode(PWM_OUT_PIN, OUTPUT);
@@ -106,7 +109,9 @@ void loop(){
     uint16_t torque_obs = analogRead(ADC_PIN);
     float angle_norm = map(angle_obs, ANGLE_OBS_MIN, ANGLE_OBS_MAX, ANGLE_NORM_MIN, ANGLE_NORM_MAX)/float(ANGLE_NORM_MAX)*100.0f;
     float torque_norm = map(torque_obs, TORQUE_OBS_MIN, TORQUE_OBS_MAX, TORQUE_NORM_MIN, TORQUE_NORM_MAX)/float(TORQUE_NORM_MAX)*100.0f-torque_bias;
-    float angle_des = get_setpoint();
+    // float angle_des = get_setpoint();
+    // float angle_des = map(analogRead(POT_PIN), 0, 1023, -100, 100);
+    float angle_des = 0.0f;
 
     /*Compute control law output*/
     controller_update(&ctrl_config, &ctrl_state, angle_des, angle_norm, torque_norm);
